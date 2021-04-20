1231637ca537d5a9b3a0933e82c262e5c51558b9
FourthAnnaka <annaka@fourth.studio>
2021-04-19 22:45:45 -0400

    painting

---
+++ projects/emoji_painter/paint/script.js
@@ -0,0 +1,60 @@
+// Get the element on the page with the id canvas
+let canvas = document.querySelector("#canvas");
+let isDrawing, points = [];
+
+let currentEmoji = "🦇";
+
+// Detect the moment we press the mouse down on the canvas div
+canvas.addEventListener("mousedown", function(event){
+  isDrawing = true;
+
+
+  // Create a new emoji div on the page and set it equal to your desired emoji
+  let newEmoji = document.createElement("div");
+  newEmoji.classList.add("emoji");
+  newEmoji.innerHTML = currentEmoji;
+
+  // // Set the style of that position so that it goes where you just pressed your mouse down
+  // newEmoji.style.left = (event.pageX - 55) + "px";
+  // newEmoji.style.top = (event.pageY - 55) + "px";
+
+  points.push({ 
+    x: newEmoji.style.left = (event.pageX - 55) + "px",
+    y: newEmoji.style.top = (event.pageY - 55) + "px"
+  });
+
+  // Add that emoji to the canvas element so that it appears on the screen
+  canvas.appendChild(newEmoji);
+
+  canvas.onmousemove = function(event) {
+    if (!isDrawing) return;
+    points.push({ 
+      x: newEmoji.style.left = (event.pageX - 55) + "px",
+      y: newEmoji.style.top = (event.pageY - 55) + "px"
+    });
+  }
+  
+  
+  canvas.addEventListener("mouseup", function (event) {
+    isDrawing = false;
+  })
+
+  
+  
+})
+
+
+
+
+
+//  delete key clears anything inside #canvas
+document.addEventListener("keydown", function(pressed) {
+  if (pressed.key === "Backspace") {
+    const canvasClear = document.getElementById("canvas");
+    canvasClear.innerHTML = "";
+    console.log("Canvas cleared!");
+  } else {
+    console.log("This key does nothing.");
+  }
+
+})
